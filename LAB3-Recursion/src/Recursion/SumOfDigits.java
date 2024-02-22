8549bfad4d411c9916d96e8947e0a735cc64360f
Ziyad Saeed Alghamdi <ziyadsaeedgh@gmail.com>
2024-02-22 11:36:56 +0300

    Recursion

--- LAB3-Recursion/src/Recursion/SumOfDigits.java
+++ LAB3-Recursion/src/Recursion/SumOfDigits.java
@@ -1,5 +1,9 @@
 package Recursion;
-
+/*
+Given a non-negative integer n, return the sum of its digits recursively (no loops).
+Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6),
+while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+*/
 public class SumOfDigits {
     public static int sumOfDigits(int n) {
         // Base case: if n is less than 10, return n itself
@@ -13,11 +17,13 @@ public class SumOfDigits {
 
     public static void main(String[] args) {
         // Test cases
-        int num1 = 12345;
-        int num2 = 9876;
+        int num1 = 126;
+        int num2 = 5;
+        int num3 = -8;
 
         // Calculate and print the sum of digits for each test case
         System.out.println("Sum of digits of " + num1 + ": " + sumOfDigits(num1));
         System.out.println("Sum of digits of " + num2 + ": " + sumOfDigits(num2));
+        System.out.println("Sum of digits of " + num3 + ": " + sumOfDigits(num2));
     }
 }
